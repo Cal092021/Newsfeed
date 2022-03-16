6a8ef4f8cd842ce76a73fb644c9d114015dca3d2
Alex Calhoun <alxcalhoun@gmail.com>
2022-03-16 16:18:17 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ReactDom from 'react-dom';
+import {useEffect, UseState} from 'react';
 
-function App() {
+function App(props) {
+
+  const photos = props.photos;
+  const listItems = photos.map((photo) => <li>{photo}</li>
+  );
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Newsfeed</h1>
+      {photos.map((image) => <img key={image.src} src={image.src} />)}
+      <div className="id"></div>
+      <p>
+      </p>
     </div>
   );
 }
+
+const photos = 
+[{src:"https://picsum.photos/id/237/200/300"},
+{src:"https://picsum.photos/id/408/200/300"},
+{src:"https://picsum.photos/id/935/200/300"},
+{src:"https://picsum.photos/id/607/200/300"},
+{src:"https://picsum.photos/id/208/200/300"},
+{src:"https://picsum.photos/id/237/200/300"},
+{src:"https://picsum.photos/id/408/200/300"},
+{src:"https://picsum.photos/id/935/200/300"},
+{src:"https://picsum.photos/id/607/200/300"},
+{src:"https://picsum.photos/id/208/200/300"},
+{src:"https://picsum.photos/id/237/200/300"},
+{src:"https://picsum.photos/id/408/200/300"},
+{src:"https://picsum.photos/id/935/200/300"},
+{src:"https://picsum.photos/id/607/200/300"},
+{src:"https://picsum.photos/id/208/200/300"},
+{src:"https://picsum.photos/id/237/200/300"},
+{src:"https://picsum.photos/id/408/200/300"},
+{src:"https://picsum.photos/id/935/200/300"},
+{src:"https://picsum.photos/id/607/200/300"},
+{src:"https://picsum.photos/id/208/200/300"},
+{src:"https://picsum.photos/id/237/200/300"},
+{src:"https://picsum.photos/id/408/200/300"},
+{src:"https://picsum.photos/id/935/200/300"},
+{src:"https://picsum.photos/id/607/200/300"},
+{src:"https://picsum.photos/id/208/200/300"},
+{src:"https://picsum.photos/id/237/200/300"},
+{src:"https://picsum.photos/id/408/200/300"},
+{src:"https://picsum.photos/id/935/200/300"},
+{src:"https://picsum.photos/id/607/200/300"},
+{src:"https://picsum.photos/id/208/200/300"},
+{src:"https://picsum.photos/id/505/200/300"}
+];
+
+
+ReactDom.render(
+  <App photos={photos}/>,
+  document.getElementById('root')
+);
 
 export default App;
